5242a2a9adeb7aad446a7962787a07daad09b20a
Lucas Albers <lucas.b.albers@gmail.com>
2025-05-29 22:49:22 -0600

    remove test schedules

--- example.schedule/Schedule1_files/ShowNavBarItemLabelsAction.bridge.js
+++
@@ -1,6 +0,0 @@
-
-
-define('caglass/actions/ShowNavBarItemLabelsAction', ['caglass/caglass.webpack.bundle'], function (caglassBundle) {
-  return caglassBundle.ShowNavBarItemLabelsAction;
-});
-//# sourceMappingURL=ShowNavBarItemLabelsAction.bridge.js.map
